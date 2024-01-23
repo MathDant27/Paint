6b9dc67de509f6b32a725c30c35783e6c134ee42
Matheus Martins <matheusmartinsdb27@gmail.com>
2024-01-23 09:17:46 -0300

    css + js

---
+++ script.js
@@ -0,0 +1,19 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+const inputColor = document.querySelector(".input__color")
+const tools = document.querySelectorAll(".buttom__tool")
+const sizeButtoms = document.querySelector(".buttom__size")
+const buttomClear = document.querySelector(".buttom__clear")
+
+let brushSize = 10
+
+canvas.addEventListener("mousedown", (event) => {
+    const {clientX, clientY} = event
+    alert(clientX, clientY)
+})
+
+const draw = (x, y) => {
+    ctx.fillStyle = "#000"
+    ctx.fillRect(x, y, brushSize, brushSize);
+}
