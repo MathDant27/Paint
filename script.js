98afe950c97648f59050135338d03bb6bdbc2041
Matheus Martins <matheusmartinsdb27@gmail.com>
2024-01-25 12:00:49 -0300

    HTML bug

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const canvas = document.querySelector("canvas")
-const ctx = canvas.getContext("2d")
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
-const inputColor = document.querySelector(".input__color")
-const tools = document.querySelectorAll(".buttom__tool")
-const sizeButtoms = document.querySelector(".buttom__size")
-const buttomClear = document.querySelector(".buttom__clear")
+const inputColor = document.querySelector(".input__color");
+const tools = document.querySelectorAll(".buttom__tool");
+const sizeButtoms = document.querySelector(".buttom__size");
+const buttomClear = document.querySelector(".buttom__clear");
 
-let brushSize = 10
+let brushSize = 10;
 
 canvas.addEventListener("mousedown", (event) => {
-    const {clientX, clientY} = event
-    alert(clientX, clientY)
-})
+    const { clientX, clientY } = event;
+    draw(clientX, clientY);
+});
 
 const draw = (x, y) => {
-    ctx.fillStyle = "#000"
+    ctx.fillStyle = "#000";
     ctx.fillRect(x, y, brushSize, brushSize);
-}
+};
